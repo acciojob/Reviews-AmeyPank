fe5fcb85c5f2efbec13bbbdf3ab37e14da83898a
AmeyPank <pankantiameya@gmail.com>
2023-05-16 11:12:43 +0530

    Rendering Reviews

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
+ReactDOM.render(<App/>, document.getElementById("root"))
 
+
+// "scripts": {
+//     "test": "jest --no-color 2>output.txt",
+//     "start": "webpack-dev-server --mode development --watch --open --hot",
+//     "build": "webpack --mode production"
+//   },
+
+// "scripts": {
+//     "test": "NODE_OPTIONS=--openssl-legacy-provider jest --no-color 2>output.txt",
+//     "start": "NODE_OPTIONS=--openssl-legacy-provider webpack-dev-server --mode development --watch --open --hot",
+//     "build": "NODE_OPTIONS=--openssl-legacy-provider webpack --mode production"
+//   },
